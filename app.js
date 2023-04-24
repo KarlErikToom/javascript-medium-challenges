adf562161ee2f62b0dc81f98d16c4aa9c5a25c38
Karl <toom.karl42@gmail.com>
2023-04-24 17:32:23 +0300

    convert to zeros

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
+/*function convertToZeros(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+  return arr;
 }
 
-console.log(reverseString("cool"));
+console.log(convertToZeros([5, 100, 0, 2, 4, 5]));
+*/
+
+/*function convertToZeros(arr) {
+    return new Array(arr.length).fill(0)
+  }
+  
+  console.log(convertToZeros([5, 100, 0, 2, 4, 5]));
+  */
+
+function convertToZeros(arr) {
+  return arr.map((arr) => 0);
+}
+
+console.log(convertToZeros([5, 100, 0]));

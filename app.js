f6d8e5a8490ae6bb8eb8c05da47a1b0760d81b25
Karl <toom.karl42@gmail.com>
2023-04-25 16:02:28 +0300

    q5 done myself

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function arrSum(arr){
-   let sum = 0
-for (i =0; i < arr.length; i++){
- sum = sum + arr[i]
-}
-return sum
+function progressiveSum(num) {
+  sum = 0;
+  for (i = 1; i <= num; i++) {
+    sum = sum + i;
+  }
+  return sum;
 }
 
-console.log(arrSum([2,2,2,3]))
+console.log(progressiveSum(2));

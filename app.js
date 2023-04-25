00606a8a3cc32d1e016fe871de897ab28b5cdcf0
Karl <toom.karl42@gmail.com>
2023-04-25 15:55:11 +0300

    q4 done myself

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-function lastElem(arr) {
- return arr[arr.length -1]
+function arrSum(arr){
+   let sum = 0
+for (i =0; i < arr.length; i++){
+ sum = sum + arr[i]
 }
-console.log(lastElem([3, 2, 0, 6, 24]));
+return sum
+}
+
+console.log(arrSum([2,2,2,3]))

54b93ba2b17b8d343b10999f66295c94d2f37769
Karl <toom.karl42@gmail.com>
2023-04-25 15:40:00 +0300

    arrlength completed myself

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-/*function filterOutFalsy(num1, num2){
-if (num1 == false){
-   return num1
-}
-else {
-   return num2
-}
+function arrLength(arr){
+ return arr.length
 }
 
-console.log(filterOutFalsy("", 10 ))
-*/
-
-function filterOutFalsy(num1, num2){
-   return num1 == false ?  num1 : num2
-   }
-   console.log(filterOutFalsy(0, 5))
+console.log(arrLength([1,2,3,5]))

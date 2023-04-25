d9fae64a00fa70ebe7742e6aabd842247daa15d1
Karl <toom.karl42@gmail.com>
2023-04-25 17:09:53 +0300

    q11 GOT IT

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-function removeApples(arr) {
-   let noApples = []
+function filterOutFalsy(arr){
+ return arr.filter(elem => elem != false)
+}
+
+console.log(filterOutFalsy(['tomato', 'orange', true, false, 0]))
+
+/*function filterOutFalsy(arr) {
+  let noFalse = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== "Apples") {
-      noApples.push(arr[i])
+    if (arr[i] != false) {
+      noFalse.push(arr[i]);
     }
   }
-  return noApples
+  return noFalse;
 }
 
-console.log(removeApples(["Banana", "cucumba", "Apples"]));
-
-/*
-function removeApples(arr){
- return arr.filter(elem => elem  !=='Apples')
-}
-
-console.log(removeApples(['banana', 'Apples', 'Oranges']))*/
+console.log(filterOutFalsy(["tomato", "orange", true, false, 0]));
+*/

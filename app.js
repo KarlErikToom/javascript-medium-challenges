24865b391819527b84875d8e7663c3e708fbb301
Karl <toom.karl42@gmail.com>
2023-04-25 18:05:21 +0300

    q6 GOT IT

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function converToBoolean(arr){
-  return arr.map(elem => !!elem)
+function calcTime(seconds) {
+   let smallNumber = '0'
+  let timerMinutes = Math.floor(seconds / 60);
+  let timerSeconds = seconds % 60;
+  if (timerMinutes.toString().length ===1){
+   timerMinutes = smallNumber + timerMinutes
+
+  }
+  if (timerSeconds.toString().length === 1){
+   timerSeconds = smallNumber + timerSeconds
+  }
+
+  return timerMinutes + ":" + timerSeconds;
 }
 
-console.log(converToBoolean([500, 0, "karl", "", []]))
+console.log(calcTime(66));

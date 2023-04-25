1713e2cc9b232be533b45c080d2fd0c6288d3e5b
Karl <toom.karl42@gmail.com>
2023-04-25 16:40:55 +0300

    q8 1/3 myself

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function getMax(arr) {
-   let max = arr[0]
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i]
-    }
-  }
-  return max
+/*function reverseString(str){
+  return str.split('').reverse().join('')
 }
 
-console.log(getMax([5, 2, 50,2 ,52, 12]));
+console.log(reverseString('abc'))
+*/
+function reverseString(str) {
+  let reversedString = "";
+  for (let i = 0; i < str.length; i++) {
+    reversedString = str[i] + reversedString;
+  }
+  return reversedString;
+}
+
+console.log(reverseString("abc"));

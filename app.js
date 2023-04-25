cc48d33e663d225e94a188e10b263a68f2c95c67
Karl <toom.karl42@gmail.com>
2023-04-25 17:12:50 +0300

    q12 GOT IT

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-function filterOutFalsy(arr){
- return arr.filter(elem => elem != false)
+function converToBoolean(arr){
+  return arr.map(elem => !!elem)
 }
 
-console.log(filterOutFalsy(['tomato', 'orange', true, false, 0]))
-
-/*function filterOutFalsy(arr) {
-  let noFalse = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] != false) {
-      noFalse.push(arr[i]);
-    }
-  }
-  return noFalse;
-}
-
-console.log(filterOutFalsy(["tomato", "orange", true, false, 0]));
-*/
+console.log(converToBoolean([500, 0, "karl", "", []]))
